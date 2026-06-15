8ebe3178d83bc19897a2636d063505810a7c6ccf
Guilherme Martins Muniz <guilhermemart465@gmail.com>
2026-06-14 21:59:56 -0300

    Delete portifolio_modelo directory

--- portifolio_modelo/index.js
+++
@@ -1,9 +0,0 @@
-const botao = document.getElementById("btnCuriosidade");
-const texto = document.getElementById("surpresa");
-
-botao.addEventListener("click", function(){
-
-    texto.innerHTML =
-    "🌌 Curiosidade surpresa: escreva aqui uma curiosidade secreta sobre você!";
-
-});
